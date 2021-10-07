54a5569292d69d2ef81fc877f6fe0009e5bc1f48
vinsonf <vfernandez@shift3tech.com>
2021-10-06 17:11:53 -0700

    uploads

--- src/server/schemas/user.schema.ts
+++ src/server/schemas/user.schema.ts
@@ -3,9 +3,16 @@ import type { User } from '../../shared/models/user.model';
 const {Schema, model} = mongoose
 
 const userSchema = new Schema<User>({
+    id: {type: String, required: true},
     name: {type: String, required: true},
     username: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
 })
+
+userSchema.pre('save', function(next) {
+   
+    this.id = `${this.email ? this.email : ''}`;
+    next();
+}); 
 
 export const UserModel = model<User>('User',userSchema)
