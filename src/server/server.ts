c4876443f11f1bedd513c4eb5949bcc8e893bbe7
vinsonf <vfernandez@shift3tech.com>
2021-10-06 20:53:07 -0700

    feat(server): pretty

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,144 +1,143 @@
-
-import express from 'express';
-import cors from 'cors';
-import crypto from 'crypto';
-import path from 'path';
-import { PostModel } from './schemas/post.schema.js';
-import { UserModel } from './schemas/user.schema.js'
-import mongoose from 'mongoose';
-import multer from 'multer';
-import {GridFsStorage} from 'multer-gridfs-storage'
+import express from "express";
+import cors from "cors";
+import crypto from "crypto";
+import path from "path";
+import { PostModel } from "./schemas/post.schema.js";
+import { UserModel } from "./schemas/user.schema.js";
+import mongoose from "mongoose";
+import multer from "multer";
+import { GridFsStorage } from "multer-gridfs-storage";
 
 const app = express();
 const PORT = 3501;
 let gfs;
-const mongoURI = 'mongodb://localhost:27017/test';
-mongoose.connect(mongoURI)
-.then(() => {
-    console.log('Connected to DB Successfully');
+const mongoURI = "mongodb://localhost:27017/test";
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to DB Successfully");
     gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
-        bucketName: "uploads"
-      });
-})
-.catch(err => console.log('Failed to Connect to DB', err))
- 
+      bucketName: "uploads",
+    });
+  })
+  .catch((err) => console.log("Failed to Connect to DB", err));
+
 // Storage
 const storage = new GridFsStorage({
-    url: mongoURI,
-    file: (req, file) => {
-      return new Promise((resolve, reject) => {
-        crypto.randomBytes(16, (err, buf) => {
-          if (err) {
-            return reject(err);
-          }
-          const filename = buf.toString("hex") + path.extname(file.originalname);
-          const fileInfo = {
-            filename: new Date().toLocaleString() + file.originalname,
-            bucketName: "uploads"
-          };
-          resolve(fileInfo);
-        });
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: file.originalname,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
       });
-    }
-  });
+    });
+  },
+});
 
-  const upload = multer({
-    storage
-  });
+const upload = multer({
+  storage,
+});
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function(req, res) {
-   res.json({message:'test'});
+app.get("/", function (req, res) {
+  res.json({ message: "test" });
 });
 
 app.post("/upload", upload.single("file"), (req, res) => {
-    // res.json({file : req.file})
-    res.redirect("/");
+  // res.json({file : req.file})
+  res.redirect("/");
+});
+
+app.get("/posts", function (req, res) {
+  PostModel.find()
+    .then((data) => res.json({ data }))
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
+    });
+});
+
+app.get("/users", function (req, res) {
+  UserModel.find()
+    .then((data) => res.json({ data }))
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
+    });
+});
+app.post("/create-user", function (req, res) {
+  const { name, email, username } = req.body;
+  const user = new UserModel({
+    name,
+    username,
+    email,
   });
-  
-
-app.get('/posts', function(req,res){
-    PostModel.find()
-    .then(data => res.json({data}))
-    .catch(err => {
-        res.status(501)
-        res.json({errors: err});
-    })
-});
-
-app.get('/users', function(req,res){
-    UserModel.find()
-    .then(data => res.json({data}))
-    .catch(err => {
-        res.status(501)
-        res.json({errors: err});
-    })
-});
-app.post('/create-user', function(req,res){
-    const {name, email, username} = req.body;
-    const user = new UserModel({
-        name,
-        username,
-        email,
-    });
-    user.save()
+  user
+    .save()
     .then((data) => {
-        res.json({data});
+      res.json({ data });
     })
-    .catch(err => {
-        res.status(501);
-        res.json({errors: err});
-    })
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
+    });
 });
 
-app.post('/create-post', function(req,res){
-    const {title, body} = req.body;
-    const post = new PostModel({
-        title,
-        body,
-    });
-    post.save()
+app.post("/create-post", function (req, res) {
+  const { title, body } = req.body;
+  const post = new PostModel({
+    title,
+    body,
+  });
+  post
+    .save()
     .then((data) => {
-        res.json({data});
+      res.json({ data });
     })
-    .catch(err => {
-        res.status(501);
-        res.json({errors: err});
-    })
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
+    });
 });
 
-app.delete('/delete-user/:id', function(req, res) {
-    const _id = req.params.id;
-    UserModel.findByIdAndDelete(_id).then((data) => {
-        console.log(data);
-        res.json({data});
-    });
-})
+app.delete("/delete-user/:id", function (req, res) {
+  const _id = req.params.id;
+  UserModel.findByIdAndDelete(_id).then((data) => {
+    console.log(data);
+    res.json({ data });
+  });
+});
 
-app.put('/update-user/:id', function(req, res) {
-    console.log("Update user");
-    UserModel.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set: { name: req.body.name, email: req.body.email },
-        },
-        {
-            new: true,
-        },
-        function(err, updateUser) {
-            if(err) {
-                res.send("Error updating user");
-            }
-            else{
-                res.json(updateUser);
-            }
-        }
-    )
-})
+app.put("/update-user/:id", function (req, res) {
+  console.log("Update user");
+  UserModel.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: { name: req.body.name, email: req.body.email },
+    },
+    {
+      new: true,
+    },
+    function (err, updateUser) {
+      if (err) {
+        res.send("Error updating user");
+      } else {
+        res.json(updateUser);
+      }
+    }
+  );
+});
 
-
-app.listen(PORT, function(){
-    console.log( `starting at localhost http://localhost:${PORT}`);
-})
+app.listen(PORT, function () {
+  console.log(`starting at localhost http://localhost:${PORT}`);
+});
